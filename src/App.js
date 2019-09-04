6d8ef3fe8f3106117b034b5955cae392a94a6cb4
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-09-04 13:27:23 +0800

    [FEAT]: add form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  };
+  handleChangeUsername = e => {
+    this.setState({ username: e.target.value });
+  };
+  handleChangePassword = e => {
+    this.setState({ password: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    alert(JSON.stringify(this.state, null, 2));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            value={this.state.username}
+            onChange={this.handleChangeUsername}
+            id="username"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            value={this.state.password}
+            onChange={this.handleChangePassword}
+            id="password"
+            type="password"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;

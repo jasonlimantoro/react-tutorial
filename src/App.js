9d83df8a0e53d72ebd0c2983e76827e02b8c3cb1
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-09-06 11:36:35 +0800

    [CHORE](App): convert to class components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Counter initialCount={10} />
-      <Counter initialCount={23} />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter initialCount={10} />
+        <Counter initialCount={23} />
+      </div>
+    );
+  }
+}
+
 export default App;

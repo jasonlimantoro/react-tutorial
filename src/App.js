cc6e432da2c5248f3d0a9165e30fe2b3acf30b1a
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-09-06 15:00:30 +0800

    [FEAT]: use router for App
    Disable the TodoApp for now to avoid distraction

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import TemperatureConverter from './components/TemperatureConverter';
+import TodoApp from './components/TodoApp';
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <TemperatureConverter />
-      </Container>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/temperature">Temperature</Link>
+              </li>
+              <li>
+                <Link to="/todos">Todos</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Container>
+          <Route path="/temperature" component={TemperatureConverter} />
+          {/*<Route path="/todos" component={TodoApp} />*/}
+        </Container>
+      </Router>
     );
   }
 }
